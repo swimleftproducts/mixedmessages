58d8d73e1158c819432bf220cb45253be2995438
eric <swimleft@gmail.com>
2021-05-06 21:24:33 -0700

    working demo of browser input

--- demo.js
+++ demo.js
@@ -1,6 +1,13 @@
+//setting up test object
 const testobj ={
- m(a,b,c){
-return '${a}+${b}+${c}'
+ // setting up metho m that logs "in m" to the conosle
+    m(){
+ console.log("in m")
  },
-
+ l(a){
+   // setting up metho m that logs user supplied to the conosle
+   console.log(a)  
+ }
 }
+
+// you can call these methods by entering testobj.m() or testobj.l(" what ever you want ") in the browser console

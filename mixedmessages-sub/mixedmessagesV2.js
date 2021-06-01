9373b628be3144ce32a42e1744edf2226627c6a3
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2021-06-01 19:57:56 +0900

    Update June 1st

--- mixedmessages-sub/mixedmessagesV2.js
+++ mixedmessages-sub/mixedmessagesV2.js
@@ -8,48 +8,44 @@ function setname(name){
    userinput[0]=name;
 }
 */
-
-takeUserInput("adam","claws","car")
-function takeUserInput(name,weapon,location) {
+// to start game
+// call takeuserInput(userInput)
+//takeUserInput("adam","claws","car")
+function takeUserInput() {
     //build array from user input
-    const userString = buildUserString(name,weapon,location);
+    //const userString = buildUserString(name, weapon, location);
+    const userString = userInput;
+    //console.log(userString)
     //build random computer array from function
     const compString = generateRandomString();
-
+    //const compString = ['adam', 'claws', 'restaurant'];
     //use .join to create single word from each array built above for easy comparison
     let userForTest = makeArrayOneWord(userString)
     let compForTest = makeArrayOneWord(compString)
-    // check if user and computer strings  matches
+        // check if user and computer strings  matches
     console.log(userForTest)
     console.log(compForTest)
 
-    let outcome = checkUserInput(userForTest,compForTest);
+    let outcome = checkUserInput(userForTest, compForTest);
     // output result 
     finalToDo(outcome);
-    
+
 };
-// This is the list of [names] for the killer
-//const names=['adam', 'barb', 'biff', 'hazel', 'ray']
-// This is the list of [weapon]
-//const weapons=['gauntlet', 'claws', 'lightSaber', 'fork', 'pistol']
-// This is the list of [location]
-//const locations=['restaurant', 'beachside', 'apartment101', 'queenJet', 'car']
-//*removed
 
 // This function is a randomizer for choosing the computer generated string [name] [weapon], [location]
-function generateRandomString(){
+function generateRandomString() {
     const names = ['adam', 'barb', 'biff', 'hazel', 'ray'];
     const weapons = ['gauntlet', 'claws', 'lightSaber', 'fork', 'pistol'];
     const locations = ['restaurant', 'beachside', 'apartment101', 'queenJet', 'car'];
     compString = [];
-    compString.push(names[Math.floor(Math.random()*names.length)]);
-    compString.push(weapons[Math.floor(Math.random()*weapons.length)]);
-    compString.push(locations[Math.floor(Math.random()*locations.length)]);
+    compString.push(names[Math.floor(Math.random() * names.length)]);
+    compString.push(weapons[Math.floor(Math.random() * weapons.length)]);
+    compString.push(locations[Math.floor(Math.random() * locations.length)]);
     return compString
 }
 
-// user
-function buildUserString(name1,weapon2,location3){
+// user (June 1st, 2021 not necessary)
+function buildUserString(name1, weapon2, location3) {
     let array = [];
     array[0] = name1;
     array[1] = weapon2;
@@ -68,22 +64,24 @@ function makeArrayOneWord(array) {
 // this compares the user generated string and computer generated string if it matches
 //
 
-function checkUserInput(userString,compString) {
-    if(userString === compString) {
+function checkUserInput(userString, compString) {
+    if (userString === compString) {
         return true
-    }else{
-        return false;  
-    }   
+    } else {
+        return false;
+    }
 }
 
 // if user and computer matches, user wins and prints ("congratulations you win the game",  etc) + the computer generated string
 // else user lose and prints ("too bad killer got away", etc.) + computer generated strings.
 
-function finalToDo(outcome){
-//if true, user win
-    if(outcome) {
-        console.log("congratulations you win the game "+ compString[0] +" " +compString[1] +" " +compString[2] )
+function finalToDo(outcome) {
+    //if true, user win
+    if (outcome) {
+        document.getElementById('computer-input').innerHTML = compString[0] + " " + compString[1] + " " + compString[2];
+        document.getElementById('end-result').innerHTML = 'Congratulations, the Star Killer has been caught! Thank you for cooperating.'
     } else {
-        console.log("to bad killer got away"+ compString[0] +" " +compString[1] +" " +compString[2] )
+        document.getElementById('computer-input').innerHTML = compString[0] + " " + compString[1] + " " + compString[2];
+        document.getElementById('end-result').innerHTML = 'Too bad, the Star Killer got away...'
     }
 }

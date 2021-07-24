b1fe684c5fce18fba2edd138b5927b341419731a
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2021-07-24 11:17:35 +0900

    added offline jQuery and Menu Icon

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ assignEvents("startButton");
 $(".logo").click(function() {
     alert("Jquery success")
 })
+
+$(".menu").click(function(){
+    $(this).hide()
+    $("nav ul").css("display", "block")
+})
